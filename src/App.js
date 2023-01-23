03c43f914ec9398da8039302bec72367285294d7
ShouwangH <shouwangh82@gmail.com>
2023-01-22 23:16:26 -0600

    12223 update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,95 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Navbar from './components/Navbar';
-import { Routes, Route } from 'react-router-dom';
-import Home from './views/Home';
-import Pokemon from './views/Pokemon';
-import Authapi from './views/Authapi';
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <Navbar/>
-      <h1>Pokemon Favorites</h1>
-      <Routes>
-        <Route path='/' element= {<Home/>}/>
-        <Route path='/pokemon' element= {<Pokemon/>}/>
-        <Route path='/authapi' element= {<Authapi/>}/>
-      </Routes>
-      
+      <form action="" class="search-bar pokesearchform">
+        <input id="searchPokemon" type="search" name="search" pattern=".*\S.*" placeholder="Search pokemon..." required/>
+          <button class="search-btn" type="submit">
+            <span>Search</span>
+          </button>
+      </form>
+
+      <div className="pokeindex">
+        <div className="pokeindex-left">
+          <div className="pokeindex-left__top">
+            <div className="circle-big"></div>
+            <div className="circle-small"></div>
+          </div>
+          <div className="pokeindex-left__screen">
+            <div className="dots">
+              <span></span>
+              <span></span>
+            </div>
+            <div className="screen">
+              <img id="pokeimg" />
+            </div>
+            <div className="status">
+              <div className="status-light"></div>
+              <div className="status-sound">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+          </div>
+          <div className="pokeindex-left__buttons">
+            <div className="buttons">
+              <div className="buttons-circle"></div>
+              <div className="buttons-quad">
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+            <div className="controller">
+              <div className="controller-touch"></div>
+              <div className="controller-joystick"></div>
+            </div>
+          </div>
+        </div>
+        <div className="pokeindex-middle"></div>
+        <div className="pokeindex-right">
+          <div className="pokeindex-right__screen"></div>
+          <div className="pokeindex-right__buttons">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div className="pokeindex-right__buttons-quadruple">
+            <div className="dot">
+              <span></span>
+              <span></span>
+            </div>
+            <div className="button">
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <div className="pokeindex-right__buttons-triple">
+            <div className="button">
+              <span><img id="type1" /></span>
+              <span><img id="type2" /></span>
+            </div>
+            <div className="light"></div>
+          </div>
+          <div className="pokeindex-right__buttons-double">
+            <span id="heightfield"></span>
+            <span id="weightfield"></span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/pokeApi.js
@@ -0,0 +1,102 @@
+// call api and fetch json
+const getData = async (name) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    console.log("pokeapi called")
+    const data = await response.json()
+    return data
+}
+
+
+//html elements for data
+const createDisplay = async (img, poketype1, texturl, height, weight, poketype2) => {
+    const text = await getFText(texturl)
+    document.getElementById('pokeimg').src = img
+    const ptype1 = getType(poketype1)
+    document.getElementById('type1').src = ptype1
+    const ptype2 = getType(poketype2)
+    document.getElementById('type2').src = ptype2
+    document.querySelector(".pokeindex-right__screen").innerHTML = text
+    document.getElementById('heightfield').innerHTML = `Ht: ${height}`
+    document.getElementById('weightfield').innerHTML = `Wt: ${weight}`
+}
+
+const loadData = async (name)=> {
+    const pokemon = await getData(name)
+    console.log ("await GetData called")
+    const sprite = pokemon.sprites.front_default
+    const type1 = pokemon.types[0].type.name
+    let type2 = "none"
+    if (pokemon.types.length ==2) {
+    type2 = pokemon.types[1].type.name
+    } 
+    const fturl = pokemon.species.url
+    const height = pokemon.height
+    const weight = pokemon.weight
+    createDisplay(sprite, type1, fturl, height, weight, type2)
+}
+
+
+const getFText = async (url) =>{
+    const response = await fetch(url)
+    const data = await response.json()
+    return data.flavor_text_entries[0].flavor_text
+}
+
+
+const getType = (type) => {
+    switch(type){
+      case 'normal':
+        return 'https://upload.wikimedia.org/wikipedia/commons/a/aa/Pok%C3%A9mon_Normal_Type_Icon.svg';
+      case 'fighting':
+        return 'https://upload.wikimedia.org/wikipedia/commons/b/be/Pok%C3%A9mon_Fighting_Type_Icon.svg';
+      case 'flying':
+        return 'https://upload.wikimedia.org/wikipedia/commons/e/e0/Pok%C3%A9mon_Flying_Type_Icon.svg';
+      case 'poison':
+        return 'https://upload.wikimedia.org/wikipedia/commons/c/c4/Pok%C3%A9mon_Poison_Type_Icon.svg';
+      case 'ground':
+        return 'https://upload.wikimedia.org/wikipedia/commons/8/8d/Pok%C3%A9mon_Ground_Type_Icon.svg';
+      case 'rock':
+        return 'https://upload.wikimedia.org/wikipedia/commons/b/bb/Pok%C3%A9mon_Rock_Type_Icon.svg';
+      case 'bug':
+        return 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Pok%C3%A9mon_Bug_Type_Icon.svg';
+      case 'ghost':
+        return 'https://upload.wikimedia.org/wikipedia/commons/a/a0/Pok%C3%A9mon_Ghost_Type_Icon.svg';
+      case 'steel':
+        return 'https://upload.wikimedia.org/wikipedia/commons/3/38/Pok%C3%A9mon_Steel_Type_Icon.svg';
+      case 'fire':
+        return 'https://upload.wikimedia.org/wikipedia/commons/5/56/Pok%C3%A9mon_Fire_Type_Icon.svg';
+      case 'water':
+        return 'https://upload.wikimedia.org/wikipedia/commons/0/0b/Pok%C3%A9mon_Water_Type_Icon.svg';
+      case 'grass':
+        return 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Pok%C3%A9mon_Grass_Type_Icon.svg';
+      case 'electric':
+        return 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Pok%C3%A9mon_Electric_Type_Icon.svg';
+      case 'psychic':
+        return 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Pok%C3%A9mon_Psychic_Type_Icon.svg';
+      case 'ice':
+        return 'https://upload.wikimedia.org/wikipedia/commons/8/88/Pok%C3%A9mon_Ice_Type_Icon.svg';
+      case 'dragon':
+        return 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Pok%C3%A9mon_Dragon_Type_Icon.svg';
+      case 'dark':
+        return 'https://upload.wikimedia.org/wikipedia/commons/0/09/Pok%C3%A9mon_Dark_Type_Icon.svg';
+      case 'fairy':
+        return 'https://upload.wikimedia.org/wikipedia/commons/0/08/Pok%C3%A9mon_Fairy_Type_Icon.svg';
+      case 'none':
+        return '';    
+      default:
+        return 'https://cdn2.bulbagarden.net/upload/3/3c/UnknownIC_Big.png';
+    }
+}
+
+
+// search button functionality
+
+const form = document.querySelector('.pokesearchform')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log("event logged")
+    const name = document.getElementById("SearchPokemon")
+    console.log("Get data for:",name.value)
+    loadData(name.value)
+})
